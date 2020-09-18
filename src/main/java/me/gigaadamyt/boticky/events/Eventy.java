979ccad5ac1bc41739a8c593f84bfe8377ba8272
GIGAADAM <47576199+GIGAADAM@users.noreply.github.com>
2020-09-18 22:41:46 +0200

    Update Eventy.java

--- src/main/java/me/gigaadamyt/boticky/events/Eventy.java
+++ src/main/java/me/gigaadamyt/boticky/events/Eventy.java
@@ -12,37 +12,37 @@ import org.bukkit.event.player.PlayerToggleFlightEvent;
 public class Eventy implements Listener {
 
     @EventHandler
-    public void move(PlayerMoveEvent e) {
-        Player p = e.getPlayer();
-        if (!(p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR))) {
-            if(p.getInventory().getBoots() == null || p.getInventory().getBoots().getItemMeta() == null || p.getInventory().getBoots().getItemMeta().getDisplayName() == null){
-                p.setAllowFlight(false);
+    public void move(PlayerMoveEvent e) { // Funkce pro event
+        Player p = e.getPlayer(); // Zde získám hráče abych mohl používat p a tím si to zkrátím
+        if (!(p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR))) { // Zjištuje zda nemá CREATIVE nebo SPECTATOR
+            if(p.getInventory().getBoots() == null || p.getInventory().getBoots().getItemMeta() == null || p.getInventory().getBoots().getItemMeta().getDisplayName() == null){ // Zjištuje zda slot pro boty se nerovná null, kvuli NullPointerException to používám
+                p.setAllowFlight(false); // Vypíná hráči fly, když si boty sundá ve výšce
                 return;
             }
-            if (p.getInventory().getBoots().getItemMeta().getDisplayName().contains("§6§lBoticky")) {
-                p.setAllowFlight(true);
-                Location loc = new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(), p.getLocation().getZ());
-                p.getWorld().spawnParticle(Particle.FALLING_DUST, loc, 5);
-                p.getWorld().spawnParticle(Particle.REDSTONE, loc, 5);
+            if (p.getInventory().getBoots().getItemMeta().getDisplayName().contains("§6§lBoticky")) { // Zjištuje zda hráč má ve slotu pro boty item s jmenem "§6§lBoticky"
+                p.setAllowFlight(true); // Zapíná hráči fly
+                Location loc = new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(), p.getLocation().getZ()); // Získává lokaci hráče
+                p.getWorld().spawnParticle(Particle.FALLING_DUST, loc, 5); // Spawne particles do světa na přesnou lokaci loc, při pohybu se spawne 5x 1 particle
+                p.getWorld().spawnParticle(Particle.REDSTONE, loc, 5); // Spawne particles do světa na přesnou lokaci loc, při pohybu se spawne 5x 1 particle
             } else {
-                p.setAllowFlight(false);
+                p.setAllowFlight(false); // Vypne hráči fly
             }
         }
     }
 
 
     @EventHandler
-    public void event(PlayerToggleFlightEvent e) {
-        Player p = e.getPlayer();
+    public void event(PlayerToggleFlightEvent e) { // Funkce pro event
+        Player p = e.getPlayer(); // Zde získám hráče abych mohl používat p a tím si to zkrátím
 
-        if (!(p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR))) {
-            if(p.getInventory().getBoots() == null || p.getInventory().getBoots().getItemMeta() == null || p.getInventory().getBoots().getItemMeta().getDisplayName() == null){
+        if (!(p.getGameMode().equals(GameMode.CREATIVE) || p.getGameMode().equals(GameMode.SPECTATOR))) { // Zjištuje zda nemá CREATIVE nebo SPECTATOR
+            if(p.getInventory().getBoots() == null || p.getInventory().getBoots().getItemMeta() == null || p.getInventory().getBoots().getItemMeta().getDisplayName() == null){ // Zjištuje zda slot pro boty se nerovná null, kvuli NullPointerException to používám
                 return;
             }
-            if (p.getInventory().getBoots().getItemMeta().getDisplayName().contains("§6§lBoticky")) {
-                p.setAllowFlight(true);
+            if (p.getInventory().getBoots().getItemMeta().getDisplayName().contains("§6§lBoticky")) { // Zjištuje zda hráč má ve slotu pro boty item s jmenem "§6§lBoticky"
+                p.setAllowFlight(true); // Zapíná hráči fly
             } else {
-                p.setAllowFlight(false);
+                p.setAllowFlight(false); // Vypíná hráči fly
             }
         }
 
